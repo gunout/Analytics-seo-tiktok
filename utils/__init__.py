80d9bd917b043626a1480a5e57981fd38086d24b
Julien Talabaza <88831335+gunout@users.noreply.github.com>
2026-03-30 16:27:44 +0400

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,6 +0,0 @@
-# utils/__init__.py
-from .data_simulator import TikTokDataSimulator
-from .ia_insights import TikTokIAInsights
-from .visualizations import TikTokVisualizations
-
-__all__ = ['TikTokDataSimulator', 'TikTokIAInsights', 'TikTokVisualizations']
